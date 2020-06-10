845d9e5fabfa565b514162fea1d0e72bee69d3c5
soyun0101 <soyun0101@github.com>
2020-06-09 20:39:59 -0700

    Added AllProductsList component at all-products file. Added setView method to ProductsPage.

---
+++ client/components/all-products.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ProductItem from './product-item';
+
+export default class AllProductsList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+      message: null
+    };
+
+    this.getAllProducts = this.getAllProducts.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+
+    fetch('/api/products')
+      .then(result => result.json())
+      .then(data => this.setState({ products: data }))
+      .catch(err => this.setState({ message: err.message }));
+  }
+
+  render() {
+
+    return (
+      <div className="container">
+        <div className="row">
+          {
+            this.state.products.map(eachProductInfo => {
+              const { productId, image, name, price } = eachProductInfo;
+
+              const dividedPrice = price / 100;
+
+              return <div key={productId}>
+                <ProductItem productId={productId} image={image} name={name} price={dividedPrice} />
+              </div>;
+            })
+          }
+
+        </div>
+
+      </div>
+    );
+
+  }
+
+}
--- client/components/products-page.jsx
+++ client/components/products-page.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
 
+import AllProductsList from './all-products';
+
 export default class ProductsPage extends React.Component {
   constructor() {
     super();
 
     this.state = {
-
+      view: {
+        name: 'catalog',
+        params: {}
+      }
     };
+
+    this.setView = this.setView.bind(this);
+  }
+
+  componentDidMount() {
+    this.setView('catalog', {});
+  }
+
+  setView(name, params) {
+    this.setState({
+      view: {
+        name: name,
+        params: params
+      }
+    });
   }
 
   render() {
-    return (
-      <div>
-        <h1>THIS IS THE PRODUCTS PAGE</h1>
-      </div>
-    );
+
+    return <div>
+      {
+        (() => {
+          switch (this.state.view.name) {
+            case 'catalog': return <AllProductsList />;
+
+          }
+        })()
+      }
+
+    </div>;
+
   }
 }
